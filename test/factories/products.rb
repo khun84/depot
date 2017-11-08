bfc43cce62aa396d8e76c4a471d54cb37276cd61
khun84 <khun84@gmail.com>
2017-11-08 18:15:40 +0800

    Validation!

---
+++ test/factories/products.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :product do
+    title 'learn enough rails'
+    description 'learn enough rails to start a blog site'
+    image_url '100x100.png'
+    price 25
+  end
+end
