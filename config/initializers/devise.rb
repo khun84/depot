6b5b0e7286cccb8af1a8665fb4f3f39fd59dcef0
khun84 <khun84@gmail.com>
2017-11-14 21:27:56 +0800

    added secret_key to devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -246,7 +246,7 @@ Devise.setup do |config|
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
-  config.secret_key = ENV["SECRET_KEY_BASE"]
+  config.secret_key = ENV["SECRET_KEY_BASE"] if Rails.env == 'production'
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
