f2297368ccaf8500e33446d246422d75720c0547
khun84 <khun84@gmail.com>
2017-11-15 22:34:26 +0800

    set keep_releases in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application,     'depot'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-
+set :keep_releases,   5
 set :system_user, 'deploy'
 
 # Don't change these unless you know what you're doing
