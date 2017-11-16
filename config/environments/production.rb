726d87d0ae74edbff927b00f67ba9e6acb635540
khun84 <khun84@gmail.com>
2017-11-16 19:19:52 +0800

    configure action cable port in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'ws://13.229.93.180/cable'
+  config.action_cable.url = 'ws://13.229.93.180/cable:8081'
   config.action_cable.allowed_request_origins = [ 'http://13.229.93.180' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
